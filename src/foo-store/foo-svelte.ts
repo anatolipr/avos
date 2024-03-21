80af935591e08c6ddbc0e86a2707be2e13ad6e59
Anatoli Radulov <anatoli@impact.com>
2024-03-20 23:13:41 -0700

    svelte two way binding

--- src/foo-store/foo-svelte.ts
+++ src/foo-store/foo-svelte.ts
@@ -5,6 +5,7 @@ import { writable, type Writable } from 'svelte/store'
 export function asSvelteStore<T>(store: Foo<T>, log?: string): Writable<T> {
     const res: Writable<T> = writable();
     const unsubscribe = store.subscribe(res.set, true, log)
+    res.subscribe((s: T) => store.set(s))
     onDestroy(unsubscribe)
     return res;
 }
--- src/foo-store/js/foo-svelte.js
+++ src/foo-store/js/foo-svelte.js
@@ -4,6 +4,7 @@ import { writable } from 'svelte/store'
 export function asSvelteStore(store, log) {
   const res = writable();
   const unsubscribe = store.subscribe(res.set, true, log)
+  res.subscribe(s => store.set(s))
   onDestroy(unsubscribe)
   return res;
 }
