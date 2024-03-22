8585a608ac054c0d4363ee7a55d963742aedb014
Anatoli Radulov <anatoli@impact.com>
2024-03-21 22:48:12 -0700

    rollback vue3 two-way binding

--- src/foo-store/foo-vue.ts
+++ src/foo-store/foo-vue.ts
@@ -1,4 +1,4 @@
-import { getCurrentScope, onScopeDispose, ref, Ref, watch } from 'vue';
+import { getCurrentScope, onScopeDispose, ref, type Ref } from 'vue';
 
 import Foo from './foo';
 
@@ -6,18 +6,15 @@ import Foo from './foo';
  * Vue 3 ref wrapper for Foo
  * @param store store to get as Ref
  * @param logMessage enables lifecycle log for subscribe / unsubscribe events and prints this message with each event. eg "main recipe list store"
- * @param readonly define if the ref will support writing - eg. this ref can be used for v-model
  */
 export function storeAsRef<T>(
     store: Foo<T>,
-    logMessage?: string,
-    readonly: boolean = false
+    logMessage?: string
 ): Ref<T> {
     let state: Ref<T> = ref() as Ref<T>;
-    let updating = false;
+
     let unsubscribe = store.subscribe(
-        (value: T) => {
-            if (updating) return;
+        function (value: T) {
             if (Array.isArray(value)) {
                 state.value = <T>[...value];
             } else if (typeof value === 'object') {
@@ -28,14 +25,6 @@ export function storeAsRef<T>(
         true,
         logMessage
     );
-
-    if (! readonly) {
-        watch(state.value, (nv: any) => {
-            updating = true;
-            store.set(nv);
-            updating = false;
-        }, {immediate: true})
-    }
 
     getCurrentScope() && onScopeDispose(unsubscribe);
 
--- src/foo-store/js/foo-vue.js
+++ src/foo-store/js/foo-vue.js
@@ -1,19 +1,16 @@
-import { getCurrentScope, onScopeDispose, ref, watch } from 'vue';
+import { getCurrentScope, onScopeDispose, ref } from 'vue';
 
 /**
  * Vue 3 ref wrapper for Foo
- * @param {Foo<T>} store Store to get as Ref
- * @param {string} [logMessage] Enables lifecycle log for subscribe / unsubscribe events and prints this message with each event. eg "main recipe list store"
- * @param {boolean} [readonly=false] Define if the ref will support writing - eg. this ref can be used for v-model
- * @template T
- * @returns {Ref<T>} Ref object
+ * @param {Foo<T>} store - store to get as Ref
+ * @param {string} [logMessage] - enables lifecycle log for subscribe / unsubscribe events and prints this message with each event. eg "main recipe list store"
+ * @returns {Ref<T>} - Ref object
  */
-export function storeAsRef(store, logMessage, readonly = false) {
+export function storeAsRef(store, logMessage) {
   let state = ref();
-  let updating = false;
+
   let unsubscribe = store.subscribe(
-    (value) => {
-      if (updating) return;
+    function (value) {
       if (Array.isArray(value)) {
         state.value = [...value];
       } else if (typeof value === 'object') {
@@ -24,14 +21,6 @@ export function storeAsRef(store, logMessage, readonly = false) {
     true,
     logMessage
   );
-
-  if (!readonly) {
-    watch(state.value, (nv) => {
-      updating = true;
-      store.set(nv);
-      updating = false;
-    }, { immediate: true });
-  }
 
   getCurrentScope() && onScopeDispose(unsubscribe);
 
