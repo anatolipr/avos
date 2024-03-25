23f1861ea73bb35e39a8e4f3dcaa01bf7cec5840
Anatoli Radulov <anatoli@impact.com>
2024-03-25 07:53:45 -0700

    fixes

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import Foo from './src/foo-store/js/foo.js'
 import { updateFromStore } from './src/foo-store/js/foo-vue2.js'
-import { asSvelteStore } from './src/foo-store/js/foo-svelte.js'
 import { storeAsRef } from "./src/foo-store/js/foo-vue.js";
 
-export { Foo, updateFromStore, asSvelteStore, storeAsRef };
+export { Foo, updateFromStore, storeAsRef };
