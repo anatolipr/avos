51c9b279b458e54ee1a15a2d76b5a1c2ea16bc2a
Anatoli Radulov <anatoli@radulov.com>
2026-01-26 09:15:43 -0800

    better namespace for singal-watch

--- packages/avosignals/avo-singal-watch.ts
+++ packages/avosignals/avo-singal-watch.ts
@@ -3,8 +3,13 @@ import { Signal, SignalWatcher } from "./avosignals";
 import { customElement, property } from "lit/decorators.js";
 import { singalToJSON } from "./avosignals-util";
 
-@customElement("signal-watch")
-export class SignalWatch extends LitElement {
+const watcherScript = document.createElement("script")
+watcherScript.src = "https://unpkg.com/@alenaksu/json-viewer@2.1.0/dist/json-viewer.bundle.js";
+document.head.appendChild(watcherScript);
+console.log("loaded json-viewer script", watcherScript);
+
+@customElement("avo-signal-watch")
+export class AvoSignalWatch extends LitElement {
     watcher = new SignalWatcher(this);
 
     @property({ attribute: false }) accessor signals: Signal<unknown>[] = [];
@@ -29,6 +34,6 @@ export class SignalWatch extends LitElement {
 
 declare global {
     interface HTMLElementTagNameMap {
-        "signal-watch": SignalWatch;
+        "avo-signal-watch": AvoSignalWatch;
     }
 }
